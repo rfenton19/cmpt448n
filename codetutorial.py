dbd253a0e2ef0b0cbf0e4806e364f29da84496f0
rfenton19 <ryan.fenton1@marist.edu>
2025-09-02 20:29:31 -0400

    need to install python

---
+++ codetutorial.py
@@ -0,0 +1,8 @@
+qubit1 = 12
+boo = False
+Bell = "Collapse to 1" #this is a peice of text
+BellPair = "I also collapse to 1"
+my_list = [Bell, qubit1, BellPair, boo]
+print(my_list[1])
+my_list.append(137)
+print(my_list)
